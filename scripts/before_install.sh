b458ce797857a54b1f329b24729b3d1af67abd16
draw-and-deploy <draw.and.deploy@gmail.com>
2022-05-25 23:40:06 -0300

    Fixing scripts

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,6 +4,6 @@ set -e
 if [ ! -d /var/www/frontend ]; then
     mkdir -p /var/www/frontend
 else
-    rm -rf /var/www/frontend/*
-    rm -rf /var/www/frontend/.*
+    rm -rf /var/www/frontend/* 2>/dev/null
+    rm -rf /var/www/frontend/.* 2>/dev/null
 fi

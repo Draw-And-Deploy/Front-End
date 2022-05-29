ffbacf7e10e20630f9e471368102b9227b272b5a
draw-and-deploy <draw.and.deploy@gmail.com>
2022-05-28 21:32:03 -0300

    Updating before_install

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+echo "Clearing the directory"
 if [ ! -d /var/www/frontend ]; then
     mkdir -p /var/www/frontend
 else
     rm -rf /var/www/frontend/* 2>/dev/null
     rm -rf /var/www/frontend/.* 2>/dev/null
 fi
+echo "Copying files to /var/www/frontend/ directory"
+cp -r frontend/build/* /var/www/frontend/

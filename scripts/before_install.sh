215f902dc89700bdc76f95a7587e533c059a78d2
Draw And Deploy <102124445+Draw-And-Deploy@users.noreply.github.com>
2022-05-30 16:26:23 -0300

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-echo "Clearing the directory"
-if [ ! -d /var/www/frontend ]; then
-    mkdir -p /var/www/frontend
-else
-    rm -rf /var/www/frontend/* 2>/dev/null
-    rm -rf /var/www/frontend/.* 2>/dev/null
-fi
+sudo apt install nginx -y
